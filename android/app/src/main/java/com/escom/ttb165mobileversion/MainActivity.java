b81a6dd6d4beb2d622eafaed87d4daea6cfec954
jisrachamp <gisrael.ramx@gmail.com>
2025-11-28 10:42:23 -0600

    Add initial Android project structure
    Introduces the Android platform directory with Gradle build files, configuration, resources, and source code for the TTB165MOBILEVERSION app. Includes .gitignore files, manifest, main activity, resource assets, and test files to enable Android builds and development.

---
+++ android/app/src/main/java/com/escom/ttb165mobileversion/MainActivity.java
@@ -0,0 +1,5 @@
+package com.escom.ttb165mobileversion;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.escom.ttb165mobileversion',
+  appName: 'TTB165MOBILEVERSION',
+  webDir: 'dist'
+};
+
+export default config;
